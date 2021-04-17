7f32692a21b1f0b57e54b372f36758e57372d236
ton1czech <tonyasek007@gmail.com>
2021-04-18 01:10:18 +0200

    nastavení mapy

--- src/Map.js
+++ src/Map.js
@@ -5,7 +5,7 @@ import { MapContainer, TileLayer, useMap } from "react-leaflet";
 const Map = () => {
     return (
         <div className="map">
-            <MapContainer center={[50.051, 14.251]} zoom={4} minZoom={3} scrollWheelZoom={true}>
+            <MapContainer center={[50.051, 14.251]} zoom={3} minZoom={3} scrollWheelZoom={true}>
                 <TileLayer
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
