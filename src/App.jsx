a1a61f49548474b1f34363b1d15f961c68e46bd6
ton1czech <tonyasek007@gmail.com>
2021-04-22 16:33:50 +0200

    👌IMPROVE: added chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import './css/App.css';
-import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import "./css/App.css";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
+import Chatbot from "react-chatbot-kit";
 
-import Overview from './Overview';
-import Chatbot from './Chatbot';
-import Map from './Map';
-import Graphs from './Graphs';
+import Overview from "./Overview";
+import Map from "./Map";
+import Graphs from "./Graphs";
 
 function App() {
   return (
-    <div className="app">
-      <div className="app_leftbar">
+    <div className='app'>
+      <div className='app_leftbar'>
         <Overview />
       </div>
 
-      <div className="app_main">
+      <div className='app_main'>
         <Map />
 
-        <div className="icon_arrow">
-          <ArrowDownwardIcon class='icon_arrow_itself' />
+        <div className='icon_arrow'>
+          <ArrowDownwardIcon className='icon_arrow_itself' />
         </div>
 
         <Graphs />
       </div>
 
-      <div className="app_chatbot">
-        <Chatbot />
+      <div className='app_chatbot'>
+        <Chatbot class='chatbot' />
       </div>
     </div>
   );
