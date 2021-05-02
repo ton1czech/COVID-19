ddb87211d9d06f154e33484e7be60e8cfa009005
ton1czech <tonyasek007@gmail.com>
2021-05-02 20:25:20 +0200

    👌IMPROVE: forms icon cursor pointer

--- src/ChatbotContainer.jsx
+++ src/ChatbotContainer.jsx
@@ -2,23 +2,10 @@ import React from 'react';
 import Chatbot from 'react-chatbot-kit';
 import './css/ChatbotContainer.css';
 
-const config = {
-  botName: 'Zabiják',
-  lang: 'cz',
-  customStyles: {
-    botMessageBox: {
-      backgroundColor: '#376B7E',
-    },
-    chatButton: {
-      backgroundColor: '#5ccc9d',
-    },
-  },
-};
-
 const ChatbotContainer = () => {
   return (
     <div className='chatbotContainer'>
-      <Chatbot config={config} />
+      <Chatbot />
     </div>
   );
 };
