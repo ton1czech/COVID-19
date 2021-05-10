9dd18d48540226c4522f2a49e6cb2f41f5af0b22
ton1czech <tonyasek007@gmail.com>
2021-05-10 11:35:42 +0200

    👌IMPROVE: chatbot avatar, header

---
+++ src/chatbot/components/ChatbotAvatar.jsx
@@ -0,0 +1,9 @@
+import './css/ChatbotAvatar.css';
+import '../../css/App_MOBILE.css';
+import React from 'react';
+
+const BotAvatar = () => {
+  return <div className='chatbot_avatar'>C</div>;
+};
+
+export default BotAvatar;
--- src/chatbot/config.js
+++ src/chatbot/config.js
@@ -1,8 +1,38 @@
+import React from 'react';
 import { createChatBotMessage } from 'react-chatbot-kit';
+
+import ChatbotAvatar from './components/ChatbotAvatar';
 
 const config = {
   initialMessages: [createChatBotMessage(`Hello world`)],
   botName: 'COVID19 Chatbot',
+  customComponents: {
+    botAvatar: (props) => <ChatbotAvatar {...props} />,
+    header: () => (
+      <div
+        style={{
+          position: 'relative',
+          display: 'flex',
+          backgroundColor: '#2b064d',
+          color: '#fff',
+          padding: '5px',
+          fontSize: '22px',
+          borderRadius: '5px',
+          fontWeight: '600',
+        }}
+      >
+        COVID19 CHATBOT
+      </div>
+    ),
+  },
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: '#8262a7',
+    },
+    chatButton: {
+      backgroundColor: '#2b064d',
+    },
+  },
 };
 
 export default config;
