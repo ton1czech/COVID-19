b322b9b8b7853fbce9676167ca743aa6ecbaa3d8
ton1czech <tonyasek007@gmail.com>
2021-05-11 21:21:00 +0200

    🎁NEW: onClick={show Map, hide Charts / show Charts, hide Map}

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,40 @@ import ShowChartIcon from '@material-ui/icons/ShowChart';
 import ChatbotContainer from './ChatbotContainer';
 import Overview from './Overview';
 import Map from './Map';
-import Graphs from './Graphs';
+import Charts from './Charts';
 
 function App() {
-  const [isInactive, setInactive] = useState('false');
+  const [isChatbotInactive, setChatbotInactive] = useState('false');
+  const [isMapActive, setMapActive] = useState(true);
+  const [isChartsActive, setChartsActive] = useState(false);
+
+  const setMapInactive = () => setMapActive(false);
+  const setChartsInactive = () => setChartsActive(false);
 
   const handleChatbotToggle = () => {
-    setInactive(!isInactive);
+    setChatbotInactive(!isChatbotInactive);
     document.getElementsByClassName('react-chatbot-kit-chat-input')[0].placeholder = 'Zde zadejte svůj dotaz.';
+  };
+
+  const handleMap = () => {
+    setChartsInactive();
+    setMapActive(!isMapActive);
+  };
+
+  const handleCharts = () => {
+    setMapInactive();
+    setChartsActive(!isChartsActive);
   };
 
   return (
     <div className='app'>
       <div className='app_topbar'>
-        <MapIcon class='hide icon_map' />
+        <MapIcon class='hide icon_map' onClick={handleMap} />
         <div className='topbar_text'>
           <h1>nadpis</h1>
           <p>432423</p>
         </div>
-        <ShowChartIcon class='hide icon_chart' />
+        <ShowChartIcon class='hide icon_chart' onClick={handleCharts} />
       </div>
 
       <div className='app_leftbar'>
@@ -35,17 +50,21 @@ function App() {
       </div>
 
       <div className='app_main'>
-        <Map />
+        <div className={isMapActive ? 'app_main_map mobile_active' : 'app_main_map mobile_inactive'}>
+          <Map />
+        </div>
 
         <div className='icon_arrow'>
           <ArrowDownwardIcon class='icon_arrow_itself' />
         </div>
 
-        <Graphs />
+        <div className={isChartsActive ? 'app_main_charts moible_active' : 'app_main_charts mobile_inactive'}>
+          <Charts />
+        </div>
       </div>
 
       <div className='app_chatbot'>
-        <div className={isInactive ? 'inactive' : 'active'}>
+        <div className={isChatbotInactive ? 'inactive' : 'active'}>
           <ChatbotContainer />
         </div>
         <PersonIcon class='chatbot_icon' onClick={handleChatbotToggle} />
---
+++ src/Charts.jsx
@@ -0,0 +1,22 @@
+import './css/Charts.css';
+import './css/App_MOBILE.css';
+import React from 'react';
+
+const Charts = () => {
+  return (
+    <div className='charts'>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+      <h1>TEST GRAFU</h1>
+    </div>
+  );
+};
+
+export default Charts;
