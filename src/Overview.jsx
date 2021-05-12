955cb9b4f5b0d6e25c117ecdc06adf1ae4f586e8
ton1czech <tonyasek007@gmail.com>
2021-05-12 11:27:58 +0200

    👌IMRPOVE: Overview scss

--- src/Overview.jsx
+++ src/Overview.jsx
@@ -34,14 +34,14 @@ const Overview = () => {
           Prosíme o vyplnění našeho krátkého <br /> formuláře ohledně COVIDU-19
         </h1>
         <p>(Formulář je zcela anonymní)</p>
-        <div className='forms_icon'>
+        <div className='forms_icon_container'>
           <a
             target='_blank'
             rel='noopener noreferrer'
             href='https://forms.office.com/pages/responsepage.aspx?id=AW39MGaVEEOyJReEvxFnXMjxamHwNP9Cqeem8s6Dwe1UNTVYMkpBSlBNOUs0N1dUTDBUTDY0UDNQMC4u'
           >
             <img
-              className='forms_icon_itself'
+              className='forms_icon'
               src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Microsoft_Forms_(2019-present).svg/1200px-Microsoft_Forms_(2019-present).svg.png'
               alt='Microsoft Forms icon'
             />
